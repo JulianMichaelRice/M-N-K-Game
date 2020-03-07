91ed7dc07b3b20e396ba5f30eaf1f48670d47179
Julian M. Rice <rice.m.julian@gmail.com>
2020-03-06 16:10:40 -0800

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Julian Rice on 3/6/20.
+//  Copyright © 2020 Julian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
